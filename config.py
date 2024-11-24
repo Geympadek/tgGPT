be2cd79df8dc8e70eb73d0db4a236fa79df9a95e
Geympadek <andreltsov08@outlook.com>
2024-11-24 17:08:49 +0400

    Created the base

---
+++ config.py
@@ -0,0 +1,14 @@
+TG_TOKEN = ""
+'''
+Токен соответсвующего тг бота
+'''
+
+CHARS_PER_TOKEN = 4
+"""
+How many characters there are in one token (approximation)
+"""
+TOKEN_LIMIT = 4000
+"""
+The program trim messages the if number of tokens exceeds this value
+"""
+
